72543364e77b2afef59fff367b875326824c30b1
Bassamhamid <bassamhamid222@gmail.com>
2025-04-17 14:38:38 +0300

    Update config.py

--- quran-bot/config.py
+++ quran-bot/config.py
@@ -1,1 +1,11 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+class Config:
+    TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+    WEBHOOK_URL = os.getenv('WEBHOOK_URL')
+    QURAN_API_URL = 'http://api.alquran.cloud/v1'
+    
+    # يمكن إضافة المزيد من الإعدادات هنا
